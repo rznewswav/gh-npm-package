8e330ef080f6b688e891b5b49cf8510c6101e36e
raflie <raflie@newswav.com>
2022-11-25 15:12:43 +0800

    Add create module utility

---
+++ template/module.spec.ts
@@ -0,0 +1,7 @@
+import { moduleCamelToken } from './module-kebab-token';
+
+describe('module-kebab-token', () => {
+  it('should be a function', () => {
+    expect(typeof moduleCamelToken).toEqual('function');
+  });
+});
