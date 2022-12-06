f1dcd024f1cf977cd5540b275217eee6d363ed2d
raflie <raflie@newswav.com>
2022-12-06 16:36:53 +0800

    Fix wordToPascal error

--- src/word-to-pascal/word-to-pascal.ts
+++ src/word-to-pascal/word-to-pascal.ts
@@ -1,6 +1,6 @@
 export function wordToPascal(word: string): string {
   return word
     .split(' ')
-    .map((e) => e.at(1).toLocaleUpperCase('en-gb') + e.slice(1))
+    .map((e) => e.at(0).toLocaleUpperCase('en-gb') + e.slice(1))
     .join('');
 }
